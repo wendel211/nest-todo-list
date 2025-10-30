13f41ad3d7ac1b6d81ec73fd9b2bb56672ce94a6
wendel21 <wendelmuniz04@gmail.com>
2025-10-30 10:04:08 -0300

    feat(tasks): adicionar dependências de class-transformer e class-validator; atualizar a importação de Task para usar o tipo

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,7 +11,7 @@ import {
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { Task } from './task.model';
+import type { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
