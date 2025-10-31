b1a1a42c8e2d64015b838b5fbe0bfdfb8517721d
wendel21 <wendelmuniz04@gmail.com>
2025-10-31 20:51:51 -0300

    feat(stats): implementa StatsModule, StatsService e StatsController com funcionalidade inicial

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { TasksService } from '../tasks/tasks.service';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(private tasksService: TasksService) {}
+
+  getStats() {
+    const all = this.tasksService.findAll();
+    const done = all.filter(t => t.done).length;
+    return {
+      total: all.length,
+      done,
+      pending: all.length - done,
+    };
+  }
+}
