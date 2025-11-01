369af45fbf882939c6efc4d9bf34887eb86db43a
wendel21 <wendelmuniz04@gmail.com>
2025-10-31 21:25:05 -0300

    feat(aplicativo): refatorar o AppController para retornar a mensagem inicial e os endpointsf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHome() {
+    return {
+      message: '🚀 Bem-vindo à API de Tarefas!',
+      docs: '/docs',
+      endpoints: ['/tasks', '/stats']
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { LoggerMiddleware } from './common/logger.middleware';
 import { StatsModule } from './stats/stats.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TasksModule, StatsModule],
+  controllers: [AppController],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
